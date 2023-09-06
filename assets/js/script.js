579f0748583e3e3a95f946cb9447af47d5897634
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-09-06 14:19:04 -0400

    CHanges made to api

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,18 @@ sr.reveal('.hero-text', {delay:200, origin:'top'});
 sr.reveal('.hero-img', {delay:450, origin:'top'});
 sr.reveal('.icons', {delay:500, origin:'left'});
 sr.reveal('.scroll-down', {delay:500, origin:'right'});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const loginModal = new Foundation.Reveal(document.getElementById("loginModal"));
+
+  menuIcon.addEventListener("click", function () {
+      // Handle opening the mobile menu
+  });
+
+  const loginLink = document.querySelector("[data-open='loginModal']");
+  loginLink.addEventListener("click", function (event) {
+      event.preventDefault();
+      loginModal.open();
+  });
+});
