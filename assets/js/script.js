3407f079e14bc7b574c9b2cf38c05fffb038ca49
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-09-07 12:30:22 -0400

    Added header and footer files and js to call them at each page.  I added a favicon and adjusted various pages including aboutus, identify.  I added root colors to lend consistancy throughout the site. and removed the login box structure.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
+// Function to load and insert content from external HTML files
+function loadAndInsertHTML(file, containerId) {
+  fetch(file)
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById(containerId).innerHTML = data;
+      })
+      .catch(error => {
+          console.error(`Error loading ${file}: ${error}`);
+      });
+}
+
+// Load header and footer content
+loadAndInsertHTML('header.html', 'headerContainer');
+loadAndInsertHTML('footer.html', 'footerContainer');
+
 menu.onclick = () => {
   menu.classlist.toggle('bx-x');
   navlist.classlist.toggle('open');
