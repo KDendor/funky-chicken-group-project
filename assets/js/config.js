2ffc3072325989ac53717c6bb2fbae59fa145b9e
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-09-10 19:46:55 -0400

    Added an error catch so that if/when the API fails it will default text to the description field

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-const ApiKey = "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";
+const ApiKey = "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";
 const perunialKey = "sk-iTS464edf444c5e1f2019";
