0d01f9749817eab6d0efe22bdf6eedf0951ed0cb
rodking7 <rodking7@rodking7s-imac.mynetworksettings.com>
2023-09-09 22:16:06 -0400

    login and contact pages

--- assets/js/perunial.js
+++ assets/js/perunial.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const plantNameInput = document.getElementById("plantName");
     const perunialInfoDiv = document.getElementById("perunialInfo");
     const expandedInfoDiv = document.getElementById("expandedInfo");
-    // const headerInfoDiv = document.getElementById("infoHeader");
 
     searchButton.addEventListener("click", () => {
         const plantName = plantNameInput.value;
@@ -11,35 +10,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (plantName) {
             const perunialUrl = `https://perenual.com/api/species-list?key=${perunialKey}&q=${encodeURIComponent(plantName)}`;
-
+            console.log("Perunial URL:", perunialUrl);
             fetch(perunialUrl)
-            .then(response => response.json())
-            .then(data => {
-                const plantData = data.data;
+                .then(response => response.json())
+                .then(data => {
+                    console.log("API Response:", data); // Log the response data
 
-                if (plantData.length > 0) {
-                    const firstPlant = plantData[0]; // Get the first plant
+                    const plantData = data.data;
 
-                    const perunialInfo = `
-                        <div>
-                            <p>Scientific Name: ${firstPlant.scientific_name.join(", ")}</p>
-                            <p>Watering: ${firstPlant.watering}</p>
-                            <p>Cycle: ${firstPlant.cycle}</p>
-                            <img src="${firstPlant.default_image.small_url}" alt="${firstPlant.common_name} Image">
-                        </div>
-                    `;
-                    perunialInfoDiv.innerHTML = perunialInfo;
+                    if (plantData.length > 0) {
+                        const firstPlant = plantData[0]; // Get the first plant
+
+                        const perunialInfo = `
+                            <div>
+                                <p>Scientific Name: ${firstPlant.scientific_name.join(", ")}</p>
+                                <p>Watering: ${firstPlant.watering}</p>
+                                <p>Cycle: ${firstPlant.cycle}</p>
+                                <img src="${firstPlant.default_image.small_url}" alt="${firstPlant.common_name} Image">
+                            </div>
+                        `;
+
+                        // Remove the 'hidden' class from perunialInfoDiv
+                        perunialInfoDiv.classList.remove("hidden");
+
+                        // Remove the 'hidden' class from expandedInfoDiv
+                        expandedInfoDiv.classList.remove("hidden");
+                        expandedInfoDiv.innerHTML = ""; // Clear expanded info
+
+                        // Display the retrieved plant information
+                        perunialInfoDiv.innerHTML = perunialInfo;
+                    } else {
+                        perunialInfoDiv.innerHTML = "<p>No plants found with that name.</p>";
+                        expandedInfoDiv.innerHTML = ""; // Clear expanded info
+                    }
+                })
+                .catch(error => {
+                    console.error("Error fetching plant information:", error);
+                    perunialInfoDiv.innerHTML = "<p>An error occurred while fetching plant information.</p>";
                     expandedInfoDiv.innerHTML = ""; // Clear expanded info
-                } else {
-                    perunialInfoDiv.innerHTML = "<p>No plants found with that name.</p>";
-                    expandedInfoDiv.innerHTML = ""; // Clear expanded info
-                }
-            })
-            .catch(error => {
-                console.error("Error fetching plant information:", error);
-                perunialInfoDiv.innerHTML = "<p>An error occurred while fetching plant information.</p>";
-                expandedInfoDiv.innerHTML = ""; // Clear expanded info
-            });
+                });
         } else {
             perunialInfoDiv.innerHTML = "<p>Please enter a plant name.</p>";
             expandedInfoDiv.innerHTML = ""; // Clear expanded info
